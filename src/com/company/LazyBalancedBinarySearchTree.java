873478812be62380fe3d30810c0023dd506176e6
JudeaDumont <dumontjudea2@gmail.com>
2018-05-16 21:59:04 -0600

    Documenting a redesign decision for the lbbst

--- src/com/company/LazyBalancedBinarySearchTree.java
+++ src/com/company/LazyBalancedBinarySearchTree.java
@@ -37,18 +37,81 @@ public class LazyBalancedBinarySearchTree<T> {
         if (root == null) {//todo: refactor, most the time the root is not null
             this.root = newNode;
         } else {
+            //a better way to do this is by walking the tree, adding the node, and then fixing the tree.
             if (current == null) {//lazy walk
                 current = root;
             }
 
-            if (current.compareTo(newNode) < 0 && current.left == null) { //left child
-                current.left = newNode;
-                newNode.parent = current;
-                current.weight -= 1;
-            } else if (current.compareTo(newNode) > 0 && current.right == null) { //right child
-                current.right = newNode;
-                newNode.parent = current;
-                current.weight += 1;
+            if (current.compareTo(newNode) < 0) { //left child
+                if (current.left == null) {
+                    current.left = newNode;
+                    newNode.parent = current;
+                    current.weight -= 1;
+                } else {
+                    //left child
+                    //left child is not null, current comes after new
+                    if (current.left.compareTo(newNode) < 0) {
+                        //left child
+                        //left child is not null, current comes after new
+                        //in-between parent and left child
+                        if (current.right == null) {
+                            //left child
+                            //left child is not null, current comes after new
+                            //in-between parent and left child
+                            if (current.left.right == null) {
+                                //straight line left, rotate
+
+                            } else {
+                                //walk left // push new through function call stack
+
+                            }
+                        } else {
+                            //left child
+                            //left child is not null, current comes after new
+                            //in-between parent and left child
+                            //left is not null, right is not null, replace left with new, left becomes left child of new
+                        }
+                    } else {
+                        //left child
+                        //left child is not null,
+                        //left child of left child of current
+                        if (current.right == null) {
+                            //left child
+                            //left child is not null,
+                            //left child of left child of current
+                            //right is null
+                            if (current.left.right == null) {
+                                //left child
+                                //left child is not null,
+                                //left child of left child of current
+                                //right is null
+                                //lefts right is null, rotate
+
+                            } else {
+                                //left child
+                                //left child is not null,
+                                //left child of left child of current
+                                //right is null
+                                //lefts right is not null
+
+                            }
+                        } else { //left is not null, right is not null, replace left with new, left becomes left child of new
+                            if (current.left.right == null) {//straight line left, rotate
+
+                            } else {
+
+                            }
+                        }
+                    }
+                }
+            } else if (current.compareTo(newNode) > 0) { //right child
+                if (current.right == null) {
+                    current.right = newNode;
+                    newNode.parent = current;
+                    current.weight += 1;
+                } else { //right child is not null
+
+                }
             } else if (current.left != null && current.right != null) { //todo:refactor for most the time this is true
                 if (current.left.compareTo(newNode) > 0 && current.right.compareTo(newNode) < 0) { //between
                     if (current.compareTo(newNode) < 0) { //between left and parent
